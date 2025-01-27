acb97f38482f99c06a46b97875d9873abcfb5393
Jan Maly <jan@invenus.dev>
2025-01-27 14:26:32 +0100

    feat: grouping objects

--- main.py
+++ main.py
@@ -11,14 +11,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # state updating
+        for sprite in updatable:
+            sprite.update(dt)
+
+        # drawing
         screen.fill("#000000")
-        player.draw(screen)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
