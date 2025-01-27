160dda86a8cc8476c65f2d3a3f18164f3c7081f3
Jan Maly <jan@invenus.dev>
2025-01-27 13:47:08 +0100

    feat: window draw

--- main.py
+++ main.py
@@ -2,9 +2,18 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
-def main():
-    print("Starting asteroids!")
+def main():    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill('#000000')
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
